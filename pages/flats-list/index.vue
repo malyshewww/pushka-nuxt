bb94f3da70063ccaef0c605103c4e139623c15bc
Alexey <malyshev@webshop.ru>
2025-01-13 17:15:01 +0300

    continue work with params on flats-list page

--- pages/flats-list/index.vue
+++ pages/flats-list/index.vue
@@ -20,38 +20,38 @@
 
 <script setup>
 useHead({
-   bodyAttrs: {
-      class: "page--flats-list",
-   },
+  bodyAttrs: {
+    class: "page--flats-list",
+  },
 });
 
 const route = useRoute();
 
 const currentPage = ref(route.query.page ? route.query.page : 0);
 const currentPriceMin = ref(
-   route.query["price[min]"] ? route.query["price[min]"] : "all"
+  route.query["price[min]"] ? route.query["price[min]"] : "all"
 );
 const currentPriceMax = ref(
-   route.query["price[max]"] ? route.query["price[max]"] : "all"
+  route.query["price[max]"] ? route.query["price[max]"] : "all"
 );
 const currentAreaMin = ref(
-   route.query["space[min]"] ? route.query["space[min]"] : "all"
+  route.query["space[min]"] ? route.query["space[min]"] : "all"
 );
 const currentAreaMax = ref(
-   route.query["space[max]"] ? route.query["space[max]"] : "all"
+  route.query["space[max]"] ? route.query["space[max]"] : "all"
 );
 const currentFloorMin = ref(
-   route.query["floor[min]"] ? route.query["floor[min]"] : "all"
+  route.query["floor[min]"] ? route.query["floor[min]"] : "all"
 );
 const currentFloorMax = ref(
-   route.query["floor[max]"] ? route.query["floor[max]"] : "all"
+  route.query["floor[max]"] ? route.query["floor[max]"] : "all"
 );
 
-const options = ref(
-   route.query["options[]"] ? route.query["options[]"] : "all"
-);
+// const options = ref(
+//   route.query["options[]"] ? route.query["options[]"] : "all"
+// );
 
-const optionView = ref("all");
+// const optionView = ref("all");
 
 // watch(
 //    () => route.query,
@@ -65,41 +65,44 @@ const optionView = ref("all");
 //    }
 // );
 
+const params = ref({});
+
 const runtimeConfig = useRuntimeConfig();
 const {
-   data: flatsList,
-   status,
-   refresh,
-   error,
+  data: flatsList,
+  status,
+  refresh,
+  error,
 } = await useAsyncData(
-   "flatsList",
-   () =>
-      $fetch(`${runtimeConfig.public.apiBase}/flats-list?_format=json`, {
-         params: {
-            "price[min]": currentPriceMin.value,
-            "price[max]": currentPriceMax.value,
-            "space[min]": currentAreaMin.value,
-            "space[max]": currentAreaMax.value,
-            "floor[min]": currentFloorMin.value,
-            "floor[max]": currentFloorMax.value,
-         },
-      }),
-   {
-      transform: (res) => {
-         const { breadcrumb, data, filter } = res;
-         console.log(data);
-         return {
-            breadcrumb,
-            data,
-            params: {
-               floor: filter.slider.floor,
-               price: filter.slider.price,
-               space: filter.slider.space,
-               options: filter.options,
-            },
-         };
+  "flatsList",
+  () =>
+    $fetch(`${runtimeConfig.public.apiBase}/flats-list?_format=json`, {
+      params: {
+        ...params.value,
+        //   "price[min]": currentPriceMin.value,
+        //   "price[max]": currentPriceMax.value,
+        //   "space[min]": currentAreaMin.value,
+        //   "space[max]": currentAreaMax.value,
+        //   "floor[min]": currentFloorMin.value,
+        //   "floor[max]": currentFloorMax.value,
       },
-   }
+    }),
+  {
+    transform: (res) => {
+      const { breadcrumb, data, filter } = res;
+      console.log(data);
+      return {
+        breadcrumb,
+        data,
+        params: {
+          floor: filter.slider.floor,
+          price: filter.slider.price,
+          space: filter.slider.space,
+          options: filter.options,
+        },
+      };
+    },
+  }
 );
 
 const router = useRouter();
@@ -107,48 +110,47 @@ const router = useRouter();
 const products = ref([]);
 
 const fetchData = async (
-   page,
-   priceMin,
-   priceMax,
-   areaMin,
-   areaMax,
-   floorMin,
-   floorMax,
-   options
+  page,
+  priceMin,
+  priceMax,
+  areaMin,
+  areaMax,
+  floorMin,
+  floorMax,
+  options
 ) => {
-   const {
-      data: dynamicData,
-      status,
-      error,
-   } = await useAsyncData(
-      "dynamicData",
-      () =>
-         $fetch(`${runtimeConfig.public.apiBase}/flats-list?_format=json`, {
-            params: {
-               page,
-               "price[min]": priceMin,
-               "price[max]": priceMax,
-               "space[min]": areaMin,
-               "space[max]": areaMax,
-               "floor[min]": floorMin,
-               "floor[max]": floorMax,
-               "options[]": options,
-            },
-         }),
-      {
-         transform: (res) => {
-            const { data } = res;
-            return {
-               main: {
-                  list: data,
-               },
-            };
-         },
-      }
-   );
-   return {
-      data: dynamicData.value.main.list,
-   };
+  const {
+    data: dynamicData,
+    status,
+    error,
+  } = await useAsyncData(
+    "dynamicData",
+    () =>
+      $fetch(`${runtimeConfig.public.apiBase}/flats-list?_format=json`, {
+        params: {
+          page,
+          "price[min]": priceMin,
+          "price[max]": priceMax,
+          "space[min]": areaMin,
+          "space[max]": areaMax,
+          "floor[min]": floorMin,
+          "floor[max]": floorMax,
+        },
+      }),
+    {
+      transform: (res) => {
+        const { data } = res;
+        return {
+          main: {
+            list: data,
+          },
+        };
+      },
+    }
+  );
+  return {
+    data: dynamicData.value.main.list,
+  };
 };
 
 // const loadData = async () => {
@@ -161,120 +163,134 @@ const fetchData = async (
 const arrValues = ref([]);
 
 watch(
-   () => route.query,
-   (newVal, oldVal) => {
-      console.log("old", oldVal);
-      console.log("new", newVal);
-      router.push({
-         path: route.path,
-         query: {
-            page: currentPage.value,
-            ...newVal,
-         },
-      });
-   }
+  () => route.query,
+  (newVal, oldVal) => {
+    console.log("old", oldVal);
+    console.log("new", newVal);
+    router.push({
+      path: route.path,
+      query: {
+        page: currentPage.value,
+        ...newVal,
+      },
+    });
+    console.log("route", route);
+  }
 );
 
 const loadData = async (
-   minPrice,
-   maxPrice,
-   minFloor,
-   maxFloor,
-   minArea,
-   maxArea,
-   options
+  minPrice,
+  maxPrice,
+  minFloor,
+  maxFloor,
+  minArea,
+  maxArea,
+  options
 ) => {
-   console.log("options", options);
-   currentPriceMin.value = minPrice;
-   currentPriceMax.value = maxPrice;
-   currentAreaMin.value = minArea;
-   currentAreaMax.value = maxArea;
-   currentFloorMin.value = minFloor;
-   currentFloorMax.value = maxFloor;
-   options.value = options;
-   router.push({
-      path: route.path,
-      query: {
-         page: currentPage.value,
-         "price[min]": minPrice,
-         "price[max]": maxPrice,
-         "space[min]": minArea,
-         "space[max]": maxArea,
-         "floor[min]": minFloor,
-         "floor[max]": maxFloor,
-      },
-   });
-   if (options.length > 0) {
-      router.push({
-         query: { ...route.query, "options[]": options.value },
-      });
-   }
-   refresh();
+  console.log("options", options);
+  currentPriceMin.value = minPrice;
+  currentPriceMax.value = maxPrice;
+  currentAreaMin.value = minArea;
+  currentAreaMax.value = maxArea;
+  currentFloorMin.value = minFloor;
+  currentFloorMax.value = maxFloor;
+  options.value = options;
+  router.push({
+    path: route.path,
+    query: {
+      page: currentPage.value,
+      "price[min]": minPrice,
+      "price[max]": maxPrice,
+      "space[min]": minArea,
+      "space[max]": maxArea,
+      "floor[min]": minFloor,
+      "floor[max]": maxFloor,
+    },
+  });
+  params.value = {
+    "price[min]": currentPriceMin.value,
+    "price[max]": currentPriceMax.value,
+    "space[min]": currentAreaMin.value,
+    "space[max]": currentAreaMax.value,
+    "floor[min]": currentFloorMin.value,
+    "floor[max]": currentFloorMax.value,
+  };
+
+  if (options.length > 0) {
+    router.push({
+      query: { ...route.query, "options[]": options.value },
+    });
+    params.value = {
+      ...params.value,
+      "options[]": options.value,
+    };
+  }
+  refresh();
 };
 
 const activeCard = ref(-1);
 
 const toggleDropdown = (index) => {
-   // Если открыта та же карточка, закрываем ее
-   if (activeCard.value === index) {
-      activeCard.value = -1;
-   } else {
-      // Открываем новую карточку и закрываем остальные
-      activeCard.value = index;
-   }
+  // Если открыта та же карточка, закрываем ее
+  if (activeCard.value === index) {
+    activeCard.value = -1;
+  } else {
+    // Открываем новую карточку и закрываем остальные
+    activeCard.value = index;
+  }
 };
 const closeAllDropdowns = (e) => {
-   const target = e.target;
-   if (!target.closest(".parameters-card__wrapper")) {
-      activeCard.value = -1;
-   }
+  const target = e.target;
+  if (!target.closest(".parameters-card__wrapper")) {
+    activeCard.value = -1;
+  }
 };
 onUnmounted(() => {
-   document.removeEventListener("click", closeAllDropdowns);
+  document.removeEventListener("click", closeAllDropdowns);
 });
 onMounted(() => {
-   document.addEventListener("click", closeAllDropdowns);
+  document.addEventListener("click", closeAllDropdowns);
 });
 </script>
 
 <style lang="scss" scoped>
 .flats-list .heading-main {
-   @media screen and (max-width: $md) {
-      gap: 48px 20px;
-   }
+  @media screen and (max-width: $md) {
+    gap: 48px 20px;
+  }
 }
 .flats {
-   overflow: clip;
-   &__wrapper {
-      display: grid;
-      align-items: start;
-      gap: 64px;
-      margin-top: 64px;
-      @media screen and (max-width: $xl) {
-         gap: 32px;
-         margin-top: 40px;
-      }
-   }
-   &__body {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      gap: 40px;
-      @media screen and (max-width: $xxxl) {
-         grid-template-columns: repeat(3, 1fr);
-      }
-      @media screen and (max-width: $xl) {
-         grid-template-columns: repeat(2, 1fr);
-         gap: 20px;
-      }
-      @media screen and (max-width: $md) {
-         grid-template-columns: 100%;
-      }
-   }
-   &__bottom {
-      justify-self: center;
-      @media screen and (max-width: $xl) {
-         justify-self: stretch;
-      }
-   }
+  overflow: clip;
+  &__wrapper {
+    display: grid;
+    align-items: start;
+    gap: 64px;
+    margin-top: 64px;
+    @media screen and (max-width: $xl) {
+      gap: 32px;
+      margin-top: 40px;
+    }
+  }
+  &__body {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 40px;
+    @media screen and (max-width: $xxxl) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (max-width: $xl) {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 20px;
+    }
+    @media screen and (max-width: $md) {
+      grid-template-columns: 100%;
+    }
+  }
+  &__bottom {
+    justify-self: center;
+    @media screen and (max-width: $xl) {
+      justify-self: stretch;
+    }
+  }
 }
 </style>
